255636b4fcaa2d4ff268332e6244b30dfa7897c4
Denis Kutovskiy <denis@kutovskiy.ru>
2025-02-16 15:57:02 +0300

    Added UniStyles import; changed name and version

--- src/extension.ts
+++ src/extension.ts
@@ -11,78 +11,139 @@ const insertOrder = {
 };
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand("react-native-move-styles.helloWorld", async () => {
-    const { activeTextEditor, showInputBox, showInformationMessage } = vscode.window;
+  const disposable = vscode.commands.registerCommand(
+    "react-native-move-unistyles.helloWorld",
+    async () => {
+      const { activeTextEditor, showInputBox, showInformationMessage } =
+        vscode.window;
 
-    /** Check if there is an active text editor  */
-    if (!activeTextEditor) return;
+      /** Check if there is an active text editor  */
+      if (!activeTextEditor) return;
 
-    /** Prompt the user to input a name for their styles */
-    const userStylesName = await showInputBox({ prompt: "Please insert your styles name" });
+      /** Prompt the user to input a name for their styles */
+      const userStylesName = await showInputBox({
+        prompt: "Please insert your styles name",
+      });
 
-    /** If the user didn't provide a style name, show an information message and return */
-    if (!userStylesName) {
-      showInformationMessage("react-native-move-styles expects the style name");
-      return;
-    }
-
-    /** Retrieve the insert order configuration from settings */
-    const configuration = vscode.workspace.getConfiguration("react-native-move-styles");
-    const userInsertOrder = configuration.get("insertOrder") || insertOrder.start;
-
-    const { document, selection } = activeTextEditor;
-    const editorCode = document.getText();
-    const stylesText = document.getText(selection);
-
-    /** If no styles were selected, show an information message and return */
-    if (!stylesText) {
-      showInformationMessage("react-native-move-styles styles to be selected");
-      return;
-    }
-
-    activeTextEditor.edit((edit) => {
-      /** Replace the selected styles in the editor with styles.userStylesName */
-      edit.replace(selection, `styles.${userStylesName}`);
-
-      /**
-       * if there is no StyleSheet.create in the editor code
-       * create a new styles object, and insert the new StyleSheet.create at the end of the documen
-       */
-      if (!editorCode.includes("StyleSheet.create")) {
-        const newStyleSheet = withStyleSheetCreate(userStylesName, formatStyles(stylesText));
-        edit.insert(new vscode.Position(document.lineCount + 1, 0), newStyleSheet);
+      /** If the user didn't provide a style name, show an information message and return */
+      if (!userStylesName) {
+        showInformationMessage(
+          "react-native-move-unistyles expects the style name"
+        );
         return;
       }
 
-      /**
-       * add the existing styles to the start or end of the StyleSheet object
-       * depending on the user preferences.
-       */
+      /** Retrieve the insert order configuration from settings */
+      const configuration = vscode.workspace.getConfiguration(
+        "react-native-move-unistyles"
+      );
+      const userInsertOrder =
+        configuration.get("insertOrder") || insertOrder.start;
 
-      /** Find the line index of the "StyleSheet.create" statement */
-      const startOfStyleSheetIdx = document.positionAt(editorCode.indexOf("StyleSheet.create")).line;
+      const { document, selection } = activeTextEditor;
+      const editorCode = document.getText();
+      const stylesText = document.getText(selection);
 
-      /** add to the start of the StyleSheet */
-      if (userInsertOrder === insertOrder.start) {
-        const stylesToBeAppended = `  ${userStylesName}: ${formatStyles(stylesText)},\n`;
-        edit.insert(new vscode.Position(startOfStyleSheetIdx + 1, 0), stylesToBeAppended);
+      /** If no styles were selected, show an information message and return */
+      if (!stylesText) {
+        showInformationMessage(
+          "react-native-move-unistyles styles to be selected"
+        );
         return;
       }
 
-      /** Find the line index of the closing "})" of the StyleSheet object */
-      const endOfStyleSheetIdx = document.positionAt(editorCode.indexOf("})", startOfStyleSheetIdx)).line;
+      activeTextEditor.edit((edit) => {
+        /** Replace the selected styles in the editor with styles.userStylesName */
+        edit.replace(selection, `styles.${userStylesName}`);
 
-      /**
-       * If there is styles present at the end, update the "}," as well
-       * since it might not contain a comma, we better delete it, start writing from there
-       */
-      const lineToDelete = activeTextEditor.document.lineAt(endOfStyleSheetIdx - 1);
-      edit.delete(lineToDelete.rangeIncludingLineBreak);
+        /**
+         * if there is no StyleSheet.create in the editor code
+         * create a new styles object, and insert the new StyleSheet.create at the end of the documen
+         */
+        if (!editorCode.includes("StyleSheet.create")) {
+          if (
+            !editorCode.includes(
+              'import { StyleSheet } from "react-native-unistyles"'
+            )
+          ) {
+            const importStatements = editorCode.match(/import .+ from .+;/g);
+            if (importStatements) {
+              const lastImportStatement = importStatements.pop();
+              if (lastImportStatement) {
+                const lastImportIndex =
+                  document.positionAt(
+                    editorCode.lastIndexOf(lastImportStatement) +
+                      lastImportStatement.length
+                  ).line + 1;
+                edit.insert(
+                  new vscode.Position(lastImportIndex, 0),
+                  'import { StyleSheet } from "react-native-unistyles";\n'
+                );
+              }
+            } else {
+              edit.insert(
+                new vscode.Position(0, 0),
+                'import { StyleSheet } from "react-native-unistyles";\n'
+              );
+            }
+          }
+          const newStyleSheet = withStyleSheetCreate(
+            userStylesName,
+            formatStyles(stylesText)
+          );
+          edit.insert(
+            new vscode.Position(document.lineCount + 1, 0),
+            newStyleSheet
+          );
+          return;
+        }
 
-      const stylesToBeAppended = `  },\n  ${userStylesName}: ${formatStyles(stylesText)},\n`;
-      edit.insert(new vscode.Position(endOfStyleSheetIdx - 1, 0), stylesToBeAppended);
-    });
-  });
+        /**
+         * add the existing styles to the start or end of the StyleSheet object
+         * depending on the user preferences.
+         */
+
+        /** Find the line index of the "StyleSheet.create" statement */
+        const startOfStyleSheetIdx = document.positionAt(
+          editorCode.indexOf("StyleSheet.create")
+        ).line;
+
+        /** add to the start of the StyleSheet */
+        if (userInsertOrder === insertOrder.start) {
+          const stylesToBeAppended = `  ${userStylesName}: ${formatStyles(
+            stylesText
+          )},\n`;
+          edit.insert(
+            new vscode.Position(startOfStyleSheetIdx + 1, 0),
+            stylesToBeAppended
+          );
+          return;
+        }
+
+        /** Find the line index of the closing "})" of the StyleSheet object */
+        const endOfStyleSheetIdx = document.positionAt(
+          editorCode.indexOf("})", startOfStyleSheetIdx)
+        ).line;
+
+        /**
+         * If there is styles present at the end, update the "}," as well
+         * since it might not contain a comma, we better delete it, start writing from there
+         */
+        const lineToDelete = activeTextEditor.document.lineAt(
+          endOfStyleSheetIdx - 1
+        );
+        edit.delete(lineToDelete.rangeIncludingLineBreak);
+
+        const stylesToBeAppended = `  },\n  ${userStylesName}: ${formatStyles(
+          stylesText
+        )},\n`;
+        edit.insert(
+          new vscode.Position(endOfStyleSheetIdx - 1, 0),
+          stylesToBeAppended
+        );
+      });
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
